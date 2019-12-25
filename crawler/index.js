d7071e85250af6452f28f8f56894986300dfbde0
Daniel Wang <daniel@loopring.org>
2019-12-25 18:30:40 +0800

    more

--- crawler/index.js
+++ crawler/index.js
@@ -1,31 +1,59 @@
 const Web3 = require('web3');
 const fs = require('fs');
-const exchangeAbi = JSON.parse(fs.readFileSync('./IExchangeV3.abi').toString());
+const mongoose = require('mongoose');
+
+const settlements = require('./settlements.js');
+
+const loopringAbi = JSON.parse(fs.readFileSync('./abi/ILoopringV3.abi').toString());
+const registryAbi = JSON.parse(fs.readFileSync('./abi/IUniversalRegistry.abi').toString());
+const exchangeAbi = JSON.parse(fs.readFileSync('./abi/IExchangeV3.abi').toString());
 
 // console.log(exchangeAbi);
 
 const methodMap = {
     "0x6cfd80c9": "updateAccountAndDeposit",
-    "0x109916fe":"verifyBlocks"
+    "0x109916fe": "verifyBlocks"
 };
 
-const main = async (exchangeAddr, blockHeight) => {
+const getExchanges = async (registryContract) => {
+    return registryContract.methods.exchanges(0).call();
+}
+
+const main = async (loopringAddr, registryAddr, exchangeAddr, blockHeight) => {
+    settlements.info("hi");
+
     const web3 = new Web3('http://18.162.247.214:8545');
-    const exchange = new web3.eth.Contract(exchangeAbi, exchangeAddr);
+    const loopring = new web3.eth.Contract(loopringAbi, loopringAddr);
+    const registry = new web3.eth.Contract(registryAbi, registryAddr);
 
-    const events = await exchange.getPastEvents('allEvents', {
-        fromBlock: blockHeight,
-        toBlock: blockHeight
-    });
+    // console.log(registry);
+
+    const exchanges = await getExchanges(registry);
+
+    console.log(exchanges);
 
 
-    for (var i = 0; i < events.length; i++) {
-        console.log('====================\n\n');
-        const txHash = events[i].transactionHash;
-        const tx = await web3.eth.getTransaction(txHash);
-        console.log(tx.input.slice(0, 10));
-    }
+
+    // const exchange = new web3.eth.Contract(exchangeAbi, exchangeAddr);
+
+    // const events = await exchange.getPastEvents('allEvents', {
+    //     fromBlock: blockHeight,
+    //     toBlock: blockHeight
+    // });
+
+
+    // for (var i = 0; i < events.length; i++) {
+    //     console.log('====================\n\n');
+    //     const txHash = events[i].transactionHash;
+    //     const tx = await web3.eth.getTransaction(txHash);
+    //     console.log(tx.input.slice(0, 10));
+    // }
 
 }
 
-main("0x7D3D221A8D8AbDd868E8e88811fFaF033e68E108", 9150280);
+main(
+    "0x8745074248634f37327Ee748137C8b31238002C7",
+    "0x4c2103152a1a402af283fa52903569f05477611f",
+    "0x7D3D221A8D8AbDd868E8e88811fFaF033e68E108",
+    9150280
+);
